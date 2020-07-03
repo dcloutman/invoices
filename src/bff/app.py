ffd452baf19fe8733fc5b22e176dae762d16c0b0
David Cloutman <david.cloutman@lampfire.com>
2020-07-03 11:33:17 -0700

    Added configuration file.

--- src/bff/app.py
+++ src/bff/app.py
@@ -23,12 +23,23 @@ app = Flask(__name__, template_folder=TEMPLATE_ROOT, static_folder=STATIC_ROOT)
 IndexView.register(app)
 InvoiceView.register(app)
 
+
+if not 'FLASK_HOST' in os.environ:
+    sys_logger.error("Critical environment variable 'FLASK_HOST' was not defined. Exiting.")
+    sys.exit(1)
 if not 'FLASK_PORT' in os.environ:
     sys_logger.info("Environment variable 'FLASK_PORT' was not set. Setting default value of 5000.")
     os.environ['FLASK_PORT'] = '5000'
 if not 'FLASK_DEBUG' in os.environ:
     sys_logger.info("Environment variable 'FLASK_DEBUG' was not set. Setting default value of False.")
     os.environ['FLASK_DEBUG'] = '0'
+
+
+config_was_loaded = app.config.from_envvar('APP_SETTINGS')
+
+if False == config_was_loaded:
+    sys_logger.error("Could not load configuration file.")
+    exit()
 
 
 if __name__ == '__main__':
--- src/bff/run-bff.sh
+++ src/bff/run-bff.sh
@@ -5,5 +5,5 @@ if [ -z $1 ]; then
     echo "No environment specified. Using Flask default (likely production). Pass 'development' as a parameter to run in development mode."
 fi
 
-JOB_DIFF_UI_SETTINGS=$DIR/bff.conf PYTHONDONTWRITEBYTECODE=1 FLASK_ENV=${1} FLASK_APP=app.py flask run --host=0.0.0.0
+APP_SETTINGS=$DIR/bff.conf PYTHONDONTWRITEBYTECODE=1 FLASK_ENV=${1} FLASK_APP=app.py flask run --host=0.0.0.0
 
