5480376f29e15afea50111c0105cb4662315aa21
David Cloutman <david.cloutman@lampfire.com>
2020-07-06 11:34:59 -0700

    Code cleanup on clock example code.

--- src/bff_fe/invoice-components.tsx
+++ src/bff_fe/invoice-components.tsx
@@ -2,34 +2,29 @@ import * as React from "react";
 import * as ReactDOM from "react-dom";
 
 type ClockState = {
-  time: Date
+    time: Date
 }
 
-// Clock has no properties, but the current state is of type ClockState
-// The generic parameters in the Component typing allow to pass props
-// and state. Since we don't have props, we pass an empty object.
 export class Clock extends React.Component<{}, ClockState> {
 
-  // The tick function sets the current state. TypeScript will let us know
-  // which ones we are allowed to set.
-  tick() {
-    this.setState({
-      time: new Date()
-    });
-  }
+    // Sets the current time with a date object. 
+    tick() {
+        this.setState({
+            time: new Date()
+        });
+    }
 
-  // Before the component mounts, we initialise our state
-  componentWillMount() {
-    this.tick();
-  } 
+    // Before the component mounts, initialise the clock's state
+    componentWillMount() {
+        this.tick();
+    } 
 
-  // After the component did mount, we set the state each second.
-  componentDidMount() {
-    setInterval(() => this.tick(), 1000);
-  }
+    // After the component has mounted, reset the state every second.
+    componentDidMount() {
+        setInterval(() => this.tick(), 1000);
+    }
 
-  // render will know everything!
-  render() {
-    return (<p>The current time is {this.state.time.toLocaleTimeString()}</p>)
-  }
+    render() {
+        return (<p>{this.state.time.toLocaleTimeString()}</p>)
+    }
 }
